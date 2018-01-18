61704af529af366d7d048218f1264e7750ae9058
Raymond Harvey <raymh2002@aol.com>
2018-01-18 00:10:39 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -14,7 +14,7 @@ describe('functions', () => {
   })
 
   describe('happyHolidayTo(holiday, name)', () => {
-    it('returns "Happy ${holiday}, ${name}!"', () => {
+    it('returns "Happy ${name}, ${holiday}!"', () => {
       expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
     })
   })
